7c63b910cc512c829c44864c1b2896e13d810f87
David <p5y4@laposte.net>
2025-05-05 09:46:02 +0200

    adding image service

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
+    console.log("middleware JWT utiliser");
+    
     // const token = req.headers['authorization']?.split(' ')[1]; 
     const token = req.cookies.token;
 
--- middleware/permissionMiddleware.js
+++ middleware/permissionMiddleware.js
@@ -1,6 +1,8 @@
 export const permissionMiddleware = (req, res, next) => {
+    console.log("middleware PERMISSION utiliser");
     if (req.user?.role !== "user") {
         res.status(403).json({ message: "Acces non autorisé" });
+        console.log("PAS AUTHORISE");
         return;
     }
     next()
