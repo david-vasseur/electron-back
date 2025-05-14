6be293dca76e2c50612f1657730bc910a3170c60
David <p5y4@laposte.net>
2025-05-14 09:48:23 +0200

    adding reset password service

--- middleware/resetPasswordMiddleware.js
+++ middleware/resetPasswordMiddleware.js
@@ -8,17 +8,8 @@ export const restPasswordForm = async (req, res) => {
     await verifiedUser(decoded.email); 
 
     res.render('resetPassword', { token });
-    
+
   } catch (err) {
-    res.status(400).send(`
-        <html>
-            <head>
-            <title>Lien expiré</title>
-            </head>
-            <body style="font-family: Arial, sans-serif; background: linear-gradient(45deg,rgba(42, 123, 155, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(83, 237, 232, 1) 100%); text-align: center; padding: 50px;">
-            <h1 style="color: rgb(197, 44, 44);">Lien expiré ou invalide</h1>
-            </body>
-        </html>
-    `);
+    res.status(400).render('expired');
   }
 };
