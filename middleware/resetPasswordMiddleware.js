cc776f1ae775c0125df414ff4270695fe19c3cbb
David <p5y4@laposte.net>
2025-05-14 19:07:49 +0200

    adding reset password service

--- middleware/resetPasswordMiddleware.js
+++ middleware/resetPasswordMiddleware.js
@@ -7,7 +7,10 @@ export const restPasswordForm = async (req, res) => {
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         await verifiedUser(decoded.email); 
-        res.setHeader('Cache-Control', 'no-store');
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Expires', '0');
+        res.setHeader('ETag', '');
         res.render('resetPassword', { token });
 
     } catch (error) {
