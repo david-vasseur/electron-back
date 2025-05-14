df1572c83ec4b659a0e91af8ef8806e68d17fc28
David <p5y4@laposte.net>
2025-05-15 01:35:35 +0200

    adding redis for using jti

--- middleware/resetPasswordMiddleware.js
+++ middleware/resetPasswordMiddleware.js
@@ -11,16 +11,16 @@ export const restPasswordForm = async (req, res) => {
         const isBlacklisted = await isJTIBlacklisted(jti);
 
         if (isBlacklisted) {
-            res.status(400).render('expired');
+            return res.status(400).render('expired');
         }
 
         res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
         res.setHeader('Pragma', 'no-cache');
         res.setHeader('Expires', '0');
         res.setHeader('ETag', '');
-        res.render('resetPassword', { token });
+        return res.render('resetPassword', { token });
 
     } catch (error) {
-        res.status(400).render('expired');
+        return res.status(400).render('expired');
     }
 };
