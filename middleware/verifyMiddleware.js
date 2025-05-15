846ae7f36b72fec71c8a451477525ef93a529b35
David <p5y4@laposte.net>
2025-05-15 17:44:08 +0200

    cleaning code

--- middleware/verifyMiddleware.js
+++ middleware/verifyMiddleware.js
@@ -8,19 +8,19 @@ export const verifyUser = async (req, res, next) => {
 	try {
 
 		if (!token) {
-			throw createError("Token invalide");
+			throw createError("Ticket invalide");
 		}
 
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 		if (!decoded) {
-			throw createError("Token invalide ou expiré");
+			throw createError("Ticket invalide ou expiré. Veuillez contacter un administrateur.", 400);
 		}
 
 		const verifiedDone = await verifiedUser(decoded.email); 
 
 		if (!verifiedDone) {
-			throw createError("Une erreur est survenue", 401);
+			throw createError("Une erreur est survenue.", 401);
 		}
 
 		return res.render("verified");
